667f7b0bf2fbc65c9c1e3de7f8fe64b1bc6a54b8
tlqhrm <85216782+tlqhrm@users.noreply.github.com>
2022-10-12 14:44:33 +0900

    gg

--- t1/프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++
@@ -1,5 +0,0 @@
-function solution(n) {
-    
-    return +[...String(n)].sort((a,b) => b-a).join("");
-    
-}
--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
+++
@@ -1,7 +0,0 @@
-function solution(arr, divisor) {
-    let filtered = arr.filter(v => {
-        if(!(v%divisor)) return v;
-    }).sort((a,b) => a-b) 
-    
-    return filtered.length ? filtered : [-1]
-}
