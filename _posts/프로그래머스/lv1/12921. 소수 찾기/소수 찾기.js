53fa851350962182505b9da739f7893ab6335db7
159 <85216782+tlqhrm@users.noreply.github.com>
2022-10-25 20:33:53 +0900

    [level 1] Title: 소수 찾기, Time: 57.56 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ _posts/프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2 ; i * i <= n; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j+=i){
+                arr[j] = false;
+            }
+        }
+    }
+    return arr.filter(x=>x).length
+}
