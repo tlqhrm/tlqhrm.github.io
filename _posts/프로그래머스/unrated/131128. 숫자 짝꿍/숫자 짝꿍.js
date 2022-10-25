3e18ceff70872891ba90f756f9302bfaa29ec6f9
159 <85216782+tlqhrm@users.noreply.github.com>
2022-10-26 06:22:24 +0900

    [unrated] Title: 숫자 짝꿍, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- _posts/프로그래머스/unrated/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ _posts/프로그래머스/unrated/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -2,13 +2,11 @@ function solution(X, Y) {
     let result = [];
     let mapX = new Map();
     let mapY = new Map();
-    let arrX = [...X].sort((a,b) => b-a)
-    let arrY = [...Y].sort((a,b) => b-a)
-    for(let x of arrX) {
-        mapX.set(x, mapX.get(x) ? mapX.get(x)+1 : 1);
+    for(let x of [...X]) {
+        mapX.set(x, mapX.get(x)+1 || 1);
     }
-    for(let y of arrY) {
-        mapY.set(y, mapY.get(y) ? mapY.get(y)+1 : 1);
+    for(let y of [...Y]) {
+        mapY.set(y, mapY.get(y)+1 || 1);
     }
     for(let i=9; i>-1; i--) {
         let numX = mapX.get(`${i}`);
@@ -17,8 +15,7 @@ function solution(X, Y) {
             for(let j=0; j<Math.min(numX,numY); j++) {
                 result.push(`${i}`);
             }
-            
         }
     }
-    return result.length !== 0 ? result[0] === '0' ? '0' : result.join('') : '-1'
+    return result.sort((a,b) => b-a).length !== 0 ? result[0] === '0' ? '0' : result.join('') : '-1'
 }
