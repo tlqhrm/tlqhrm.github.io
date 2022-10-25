183e516a49f33b486e47aa712633673aee1bdd32
159 <85216782+tlqhrm@users.noreply.github.com>
2022-10-25 15:56:51 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- _posts/프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ _posts/프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,15 +1,3 @@
 function solution(n, arr1, arr2) {
-    let binary1 = arr1.map(x => x.toString(2).padStart(n,'0'))
-    let binary2 = arr2.map(x => x.toString(2).padStart(n,'0'))
-    let result = [];
-    for(let i=0; i<n; i++) {
-        let pushString = "";
-        for(let j=0; j<n; j++) {
-            if(binary1[i][j] !== '0' || binary2[i][j] !== '0') {
-                pushString += '#'; 
-            }else pushString += ' ';
-        }
-        result.push(pushString)
-    }
-    return result;
+    return arr1.map((v,i) => [...(v|arr2[i]).toString(2).padStart(n,'0')].map(x => x === '0' ? ' ' : '#').join(""))
 }
