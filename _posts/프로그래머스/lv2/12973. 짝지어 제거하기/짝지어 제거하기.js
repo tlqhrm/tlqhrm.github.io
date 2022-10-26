555a07e2ff05e624bb913afca522ff43e1acd324
159 <85216782+tlqhrm@users.noreply.github.com>
2022-10-27 05:28:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.27 ms, Memory: 46.9 MB -BaekjoonHub

--- _posts/프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ _posts/프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,7 +1,7 @@
 function solution(s){
     let stack = [];
     
-    for(let i=0; i< s.length; i++ ) {
+    for( let i=0; i< s.length; i++ ) {
         if(stack[stack.length-1] === s[i]) stack.pop();
         else stack.push(s[i]);
     }
